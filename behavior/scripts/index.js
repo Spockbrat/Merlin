647b364084519d1f2a3b9c2f25a4fe1d602997db
Brittany VanPutten <brittanyvanputten@gmail.com>
2017-01-20 20:17:45 -0700

    added collectRetailer, collectProduct, provideRegulations steps

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,10 +33,61 @@ exports.handle = (client) => {
     }
   })
 
+  const collectRetailer = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().retailer)
+    },
+
+    extractInfo() {
+      const retailer = client.getFirstEntityWithRole(client.getMessagePart(), 'retailer')
+
+      if (retailer) {
+        client.updateConversationState({
+          retailer: retailer,
+        })
+
+        console.log('User wants information related to:', retailer.value)
+      }
+    },
+
+    prompt() {
+      client.addResponse('prompt/retailer')
+      client.done()
+    },
+  })
+
+  const collectProduct = client.createStep({
+    satisfied() {
+      return Boolean(client.getConversationState().product)
+    },
+
+    prompt() {
+      // Need to ask user for product type
+      client.done()
+    },
+
+  })
+
+  const provideRegulations = client.createStep({
+    satisfied() {
+      return false
+    },
+
+    prompt () {
+      //Need to provide regulations
+      client.done()
+    },
+  })
+
   client.runFlow({
     classifications: {
       // map inbound message classifications to names of streams
     },
+    streams: {
+      main: 'greeting',
+      greeting: [sayHello],
+      getRegulations: [collectRetailer, collectProduct, provideRegulations],
+    }
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
